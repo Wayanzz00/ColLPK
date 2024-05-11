e558ca4553e40b20f915cc33d704d70a3a6ab61f
Wayanzz00 <wayanraka29@gmail.com>
2024-05-11 22:48:54 +0700

    Add files via upload

--- Home.py
+++ Home.py
@@ -1,10 +1,58 @@
 import streamlit as st
+import base64
+import requests
+from streamlit_lottie import st_lottie  # Assuming you're using a Streamlit extension for Lottie animations
 
 
-st.page_link("Home.py", label="Home", icon="🏠")
-st.page_link("pages/Alkalimetri.py", label="Alkalimetri", icon="1️⃣")
-st.page_link("pages/Asidimetri.py", label="Asidimetri", icon="1️⃣")
-st.page_link("pages/Permanganometri.py", label="Permanganometri", icon="1️⃣")
-st.page_link("pages/Iodometri.py", label="Iodometri", icon="1️⃣")
-st.page_link("pages/Kompleksometri.py", label="Kompleksometri", icon="1️⃣")
-st.page_link("http://www.google.com", label="Google", icon="🌎")
+def img_to_base64(image_path):
+    """Convert image to base64"""
+    with open(image_path, "rb") as img_file:
+            return base64.b64encode(img_file.read()).decode()
+    
+    # Load image and convert to base64
+img_path = "imgs/icon_aka.png"  # Replace with the actual image path
+img_base64 = img_to_base64(img_path)
+st.sidebar.markdown(
+    f'<img src="data:image/png;base64,{img_base64}" style="width: 100%; height: auto;">',
+    unsafe_allow_html=True,
+)
+
+st.sidebar.markdown("---")
+st.sidebar.header("~~~ PROJECT LPK ~~~")
+
+# Create two columns
+col1, col2 = st.columns([1, 2])
+
+with col1 :
+    st.title (" ")
+    st.title (" ")
+    st.title("TITRIMETRI")
+
+    st.page_link("Home.py", label="Home", icon="🏠")
+    st.page_link("pages/Alkalimetri.py", label="Alkalimetri", icon="1️⃣")
+    st.page_link("pages/Asidimetri.py", label="Asidimetri", icon="2️⃣")
+    st.page_link("pages/Permanganometri.py", label="Permanganometri", icon="3️⃣")
+    st.page_link("pages/Iodometri.py", label="Iodometri", icon="4️⃣")
+    st.page_link("pages/Kompleksometri.py", label="Kompleksometri", icon="5️⃣")
+    st.page_link("http://www.google.com", label="Google", icon="🌎")
+
+# Define the URL of the Lottie file (JSON format)
+lottie_url = "https://lottie.host/cbcd7e7b-3119-45a2-862a-858ba07d3e39/TlOccKrpDV.json"
+
+# Function to load Lottie animation from URL
+def load_lottie_url(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# Load the Lottie animation
+lottie_json = load_lottie_url(lottie_url)
+
+# Display the Lottie animation
+with col2 :
+    if lottie_json is not None:
+        st_lottie(lottie_json)
+    else:
+        st.write("Failed to load Lottie animation.")
+    
--- pages/Alkalimetri.py
+++ pages/Alkalimetri.py
@@ -2,20 +2,20 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-col1, col2 = st.columns([10, 1])
-with col2:
+col1, col2, col3 = st.columns([1, 2, 0.5])
+with col3:
     st.markdown=st.page_link("Home.py", label="Home")
 
-
-st.title ("Alkalimetri")
-
+with col2:
+    st.title ("Alkalimetri")
+    
 # Fungsi untuk menghitung konsentrasi
 def konsentrasi(gram, volume, BE):
     try:
         result = gram / (volume * BE)
         return result
     except ZeroDivisionError:
-        return "Error: BE tidak boleh nol. Silakan masukkan nilai yang valid."
+        return "Error: Silakan masukkan nilai yang valid (Kolom tidak boleh nol)."
 
 # Fungsi untuk menghasilkan jawaban dari input pengguna
 def materi_utama(materi):
@@ -35,95 +35,96 @@ def materi_utama(materi):
         ~ N adalah Normalitas (mgrek/mL)
         ~ BE adalah Berat ekuivalen titrat(mg/mgrek)
         ~ V adalah Volume akhir titrasi (mL)"""
-
     }
 
     return options.get(materi)
 
 # Fungsi untuk menghitung mean dan standard deviation
 def data_pengamatan(df, nama_column, mean, std_dev):
-    # Extract the specified column
+    # Ekstrak Spesifik Kolom
     column_data = df[nama_column]
 
-    # Evaluate the mean and standard deviation functions
-    hasil_mean = eval(mean)(column_data)
-    hasil_std_dev = eval(std_dev)(column_data)
+    # Fungsi untuk menghitung Mean, Standar Deviasi dan %RSD
+    hasil_mean = np.mean(column_data)
+    hasil_std_dev = np.std(column_data)
     hasil_rsd = (hasil_std_dev / hasil_mean) * 100 if hasil_mean != 0 else 0
     
-     # Round the results to four decimal places
+     # Desimal Hasil Akhir
     hasil_mean = round(hasil_mean, 4)
     hasil_rsd = round(hasil_rsd, 2)
     hasil_std_dev = round(hasil_std_dev, 6)
 
-    # Display the results
-    st.write(f"Rata-rata dari {nama_column}: {hasil_mean}")
-    st.write(f"Standar Deviasi dari {nama_column}: {hasil_std_dev}")
-    st.write(f"%RSD dari {nama_column}: {hasil_rsd}%")
+    # Menampilkan Hasil Kalkulasi
+    st.success(f"Rata-rata dari {nama_column}: {hasil_mean}")
+    st.success(f"Standar Deviasi dari {nama_column}: {hasil_std_dev}")
+    st.success(f"%RSD dari {nama_column}: {hasil_rsd}%")
     
 def main():
     
     # Pilihan konten menggunakan selection box
     selected_content = st.radio(
-        "Pilih konten:",
-        ["Materi Utama", "Kalkulator Konsentrasi", "Data Pengamatan"],
+        "Pilih konten :",
+        ["Materi Utama", "Kalkulator Konsentrasi", "Kalkulasi %RSD"],
         horizontal=True,
     )
 
-    # Tampilkan konten terkait
+    # Tampilk konten terkait
     if selected_content == "Kalkulator Konsentrasi":
         st.header("Kalkulator Konsentrasi")
-        gram = st.number_input("Masukkan jumlah massa (mg):", min_value=0.0, format="%.4f")
-        volume = st.number_input("Masukkan volume titran (mL):", min_value=0.0)
-        BE = st.number_input("Masukkan nilai BE Titrat (mg/mgrek):", min_value=0.00)
+        gram = st.number_input("Masukkan jumlah massa (mg):", min_value=0.00)
+        volume = st.number_input("Masukkan volume (mL):", min_value=0.00)
+        BE = st.number_input("Masukkan nilai BE Titrat:", min_value=0.00)
         if st.button("Kalkulasi"):
-            result = konsentrasi(gram, volume, BE)
-            result_rounded = round(result, 4)
-            st.write(f"Hasil perhitungan Normalitas : {result_rounded} N")
+            if gram == 0 or volume == 0 or BE == 0:
+                st.error("Pastikan untuk mengisi semua nilai.")
+            else:
+                result = konsentrasi(gram, volume, BE)
+                result_rounded = round(result, 4)
+                st.success(f"Hasil perhitungan Normalitas : {result_rounded} N")
             
     elif selected_content == "Materi Utama":
         st.header("Materi Utama")
         user_input = st.selectbox("Pilih topik : ", ["Pengertian Alkalimetri", "Pemilihan Indikator", "Reaksi Umum", "Rumus Perhitungan Normalitas"])
         if st.button("Kirim"):
             response = materi_utama(user_input)
-            text_area_height = min(max(len(response.split('\n')) * 25, 200), 600)  # Adjust height dynamically
-            st.text_area("Penjawab:", value=response, height=text_area_height, disabled=True)
+            text_area_height = min(max(len(response.split('\n')) * 20, 200), 600)  # Adjust height dynamically
+            st.text_area("💸 Penjawab", value=response, height=text_area_height, disabled=True)
         
       
-    elif selected_content == "Data Pengamatan":
-        st.title("Data Pengamatan")
+    elif selected_content == "Kalkulasi %RSD":
+        st.header("Kalkulasi %RSD")
 
-        # Sample DataFrame
+        # Table Data Pengamatan
         data = {
-            "Sample (mg)": [0.01, 0.02, 0.03],
+            "Sample (mg)": [0.00, 0.00, 0.00],
             "Volume Titran (mL)": [0.00, 0.00, 0.00],
             "Normalitas Titran (N)": [0.00, 0.00, 0.00]
         }
         df = pd.DataFrame(data)
 
-        # Allow users to input the column index
-        column_index = st.sidebar.selectbox("Pilih kolom untuk dikalkulasi :", df.columns)
+        # Definisi dari fungsi
+        mean = st.write()
+        std_dev = st.write()
 
-        # Allow users to input functions for mean and standard deviation
-        st.sidebar.header("Kalkulasi Data Statistik")
-        mean = st.sidebar.text_input("Fungsi Rata - rata :", value="np.mean")
-        std_dev = st.sidebar.text_input("Fungsi Standar Deviasi :", value="np.std")
+        # Input judul kolom untuk dikalkulasi
+        column_index = st.selectbox("Pilih kolom untuk dikalkulasi :", df.columns)
 
-        # Allow users to edit a specific column
-        st.sidebar.header("Edit Column")
+        # Edit Spesifik Kolom
+        st.sidebar.header("Edit Specific Column")
         selected_columns = []
         for column in df.columns:
-            if st.sidebar.checkbox(f"Edit '{column}'", value=True, key=column):
+            if st.sidebar.checkbox(f"Edit {column}", value=True, key=column):
                 selected_columns.append(column)
 
         updated_df = df.copy()
         if len(selected_columns) > 0:
             df_edit = df[selected_columns]
             edited_df = st.data_editor(df_edit)
-            if st.sidebar.button("Kalkulasi"):
+            if st.button("Kalkulasi"):
                 for column in selected_columns:
                     updated_df[column] = edited_df[column]
 
-                # Calculate and display mean and standard deviation
+                # Kalkulasi melalui fungsi data_pengamatan
                 data_pengamatan(updated_df, column_index, mean, std_dev)
 
 if __name__ == "__main__":
